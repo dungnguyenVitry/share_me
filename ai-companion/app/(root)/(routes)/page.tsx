ee302313eb8790d61ef7466f3a24e788ab8e67bd
dungnguyenVitry <dunglannion@protonmail.com>
2023-10-04 16:09:19 +0200

    filter categories

--- ai-companion/app/(root)/(routes)/page.tsx
+++ ai-companion/app/(root)/(routes)/page.tsx
@@ -1,9 +1,12 @@
+import { Categories } from "@/components/categories";
 import { SearchInput } from "@/components/searchinput";
-import { UserButton } from "@clerk/nextjs";
-const RootPage = () => {
+import prismadb from "@/lib/prismadb"
+const RootPage = async () => {
+    const categories = await prismadb.category.findMany();
     return (
         <div className=" h-full p-4 space-y-2">
-           <SearchInput/>
+            <SearchInput />
+            <Categories data={categories}/>
         </div>
     )
 }
